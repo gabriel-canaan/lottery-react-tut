b884a6ae14a2b1d17d3acc58d87352c2b1b705a5
gabriel <777canaan@gmail.com>
2018-01-21 22:51:58 +1300

    set up form to enter lottery

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: ''
   };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
@@ -24,6 +25,18 @@ class App extends Component {
         This contract is managed by {this.state.manager}.
         There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether !
       </p>
+      <hr />
+      <form>
+        <h4>Want to try your luck</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
     </div>
     );
   }
